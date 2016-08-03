395484cd6136828dcf5e0d86f79d30279c3eb2bf
pxai <pello_altadill@cuatrovientos.org>
2016-08-03 17:35:21 +0200

    based in new project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+
 import { AppComponent } from './app/app.component';
-if (process.env.ENV === 'production') {
+import { APP_ROUTER_PROVIDERS } from './app/app.routes';
+
+// depending on the env mode, enable prod mode or add debugging modules
+if (process.env.ENV === 'build') {
   enableProdMode();
 }
-bootstrap(AppComponent, []);
+
+bootstrap(AppComponent, [
+    // These are dependencies of our App
+    HTTP_PROVIDERS,
+    APP_ROUTER_PROVIDERS
+  ])
+  .catch(err => console.error(err));
